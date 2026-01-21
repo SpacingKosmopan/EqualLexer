b362c43449e0cd58f5ccb2082b190b39cf7530fa
Jakub Starmach <starmach.jakub@lo2rabka.nowotarski.edu.pl>
2026-01-21 20:34:04 +0100

    Initialized repo, begun working

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+void main() throws IOException {
+    Path srcPath = Path.of("src.eql");
+    if (!Files.exists(srcPath, LinkOption.NOFOLLOW_LINKS)) {
+        System.out.println("[!] Source file does not exist!");
+        return;
+    }
+    String source = Files.readString(Path.of("src.eql"));
+    if (source.equals("")) {
+        System.out.println("[!] Source file is empty");
+        return;
+    }
+    char[] chars = source.toCharArray();
+
+    int pos = 0;
+
+    while (pos < chars.length) {
+        char c = chars[pos];
+
+        pos++;
+    }
+
+}
